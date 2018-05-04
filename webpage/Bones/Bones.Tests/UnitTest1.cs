59ed0c77fed92fc9de3337ee4b840b4f09d57140
Unknown <martinezlopez.mau@gmail.com>
2018-05-03 23:38:55 -0600

    page

---
+++ webpage/Bones/Bones.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bones.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ webpage/Bones/Bones/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bones.Global" Language="C#" %>
